b98bc3663393d88817a1198f74a01c2bd09c74f3
tshmieldev <wojtekrok88@wp.pl>
2023-12-07 23:08:25 +0100

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+import Image from 'next/image';
+
+export default function Home() {
+	return <div className="h-[800px] mt-[400px] mb-[400px] bg-pink-400"></div>;
+}
---
+++ src/app/page2/page.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+	return <div className="h-[800px] mt-[400px] mb-[400px] bg-red-400"></div>;
+};
+
+export default page;
